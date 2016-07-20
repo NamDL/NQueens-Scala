bd582ba533336841fe2c68b62bac99e98b6f19d7
NamDL <namratha.dl@gmail.com>
2016-07-19 19:28:11 -0700

    NQueens

---
+++ src/NQueens.scala
@@ -0,0 +1,34 @@
+
+object NQueens extends App{
+
+  def queens(n:Int): Set[List[Int]]={
+    def placeQueens(k:Int):Set[List[Int]]={
+      if (k==0) Set(List())
+      else
+        for{
+          queens<-placeQueens(k-1)
+          col <- 0 until n
+          if isSafe(col,queens)
+        } yield col::queens
+    }
+    placeQueens(n)
+  }
+  
+  def isSafe(col: Int, queens: List[Int]):Boolean={
+  	val row=queens.length
+  	val queenRow=(row-1 to 0 by -1) zip queens
+  	queenRow forall{
+  		case(r,c)=> col!=c && math.abs(col-c)!= row-r
+  	}
+	}
+  
+  def show(queens:List[Int])={
+    val lines= for(col<-queens.reverse) yield Vector.fill(queens.length)("* ").updated(col,"X ").mkString 
+    "\n"+(lines mkString "\n")
+
+   }
+	
+  print((queens(4) map show) mkString "\n")  
+  
+	
+}
